6541463c35487dc5c6d6db75473f962f508a76b5
Paweł Kuffel <pawel@kuffel.io>
2022-05-25 18:25:36 +0200

    test: add invalid broker address and ignoreOnClose testing scenarios

--- index.spec.js
+++ index.spec.js
@@ -16,6 +16,60 @@ test('default values', (done) => {
         expect(err).toBeUndefined();
         expect(app.kafka).toHaveProperty('client');
         expect(app.kafka).toHaveProperty('producer');
+        app.close();
         done();
+    });
+});
+
+test('invalid broker address', async () => {
+    expect.assertions(1);
+    await expect(
+        app
+            .register(fastifyKafkaJS, {
+                config: {
+                    brokers: ['nope:9092'],
+                    clientId: 'some-client',
+                    retry: {
+                        retries: 0
+                    }
+                }
+            })
+            .ready()
+    ).rejects.toThrowError();
+    app.close();
+});
+
+test("closes the producer when ignoreOnClose is false", (done) => {
+    app.register(fastifyKafkaJS, { ignoreOnClose: false }).ready((err) => {
+        expect(err).toBeUndefined();
+        expect(app.kafka).toHaveProperty('client');
+        expect(app.kafka).toHaveProperty('producer');
+        let closed = false;
+        app.kafka.producer.on('producer.disconnect', () => {
+            closed = true;
+        });
+        app.close().then(() => {
+            expect(closed).toEqual(true);
+            done();
+        });
+    });
+});
+
+test("doesn't close the producer when ignoreOnClose option is set to true", (done) => {
+    app.register(fastifyKafkaJS, { ignoreOnClose: true }).ready((err) => {
+        expect(err).toBeUndefined();
+        expect(app.kafka).toHaveProperty('client');
+        expect(app.kafka).toHaveProperty('producer');
+        let closed = false;
+        app.kafka.producer.on('producer.disconnect', () => {
+            closed = true;
+        });
+        app.close().then(() => {
+            expect(closed).toEqual(false);
+            app.kafka.producer.disconnect().then(() => {
+                expect(closed).toEqual(true)
+                done();
+            })
+        });
     });
 });
